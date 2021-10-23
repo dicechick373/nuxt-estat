9afd8f35afa701880ec480768375e9295c074f05
daisuke-minami <daisuke.minami.1979@gmail.com>
2021-10-23 13:29:07 +0900

    test

--- components/PopulationPref.vue
+++ components/PopulationPref.vue
@@ -10,22 +10,24 @@
 
         <v-card-text>
           <v-row>
-            <v-switch
-              v-model="switch1"
-              :label="`Switch 1: ${switch1.toString()}`"
-            ></v-switch>
+            <!-- ラジオボタン -->
+            <v-radio-group v-model="selectedSeries" row>
+              <v-radio label="総数" value="all"></v-radio>
+              <v-radio label="内訳" value="break"></v-radio>
+            </v-radio-group>
 
             <v-spacer />
             <!-- ラジオボタン -->
-            <v-radio-group v-model="series" row>
+            <v-radio-group v-model="selectedChartType" row>
               <v-radio label="Column" value="column"></v-radio>
               <v-radio label="Line" value="line"></v-radio>
-              <v-radio label="Area" value="area"></v-radio>
+              <!-- <v-radio label="Area" value="area"></v-radio> -->
             </v-radio-group>
           </v-row>
           <!-- グラフ -->
           <!-- <HighchartsColumnChart :display-data="displayData" /> -->
 
+          <component :is="chartComponent" :display-data="displayData" />
           <!-- 注釈 -->
           <hr class="my-3" />
           <p>地域経済分析システムRESASのAPIを利用して</p>
@@ -39,23 +41,23 @@
 export default {
   data() {
     return {
-      resasResponse: null,
-      series: 'all',
-      switch1: true,
-      unit: '人'
+      estatResponse: null,
+      selectedSeries: 'all',
+      selectedChartType: 'column',
+      unit: '人',
     }
   },
   async fetch() {
     const params = this.estatParams
     const { data } = await this.$estat.get(null, { params })
     console.log(data)
-    this.resasResponse = data.GET_STATS_DATA
+    this.estatResponse = data
   },
   props: {
     selectedPref: {
       type: Object,
-      required: true
-    }
+      required: true,
+    },
   },
   computed: {
     prefCode() {
@@ -66,37 +68,69 @@ export default {
     },
     estatParams() {
       return {
-        // appId: '724e5b90772a3e9289f41a253e4e7e32438f4fff',
         statsDataId: '0000010101',
         cdArea: ['28000'],
-        cdCat01: ['A1101', 'A110101']
+        cdCat01: ['A1101', 'A110101', 'A110102'],
+      }
+    },
+    series() {
+      return {
+        id: 'cat01',
+        item: [
+          {
+            code: 'A1101',
+            name: '総数',
+          },
+          {
+            code: 'A110101',
+            name: '男性',
+          },
+          {
+            code: 'A110102',
+            name: '女性',
+          },
+        ],
       }
     },
     title() {
       return `${this.prefName}の人口構成`
     },
     chartData() {
-      return this.resasResponse.map(d => {
+      const series = this.series
+      const value =
+        this.estatResponse.GET_STATS_DATA.STATISTICAL_DATA.DATA_INF.VALUE
+      return series.item.map((d) => {
         return {
-          name: d.label,
-          data: d.data.map(d => {
-            return {
-              x: d.year,
-              y: d.value,
-              unit: this.unit
-            }
-          })
+          name: d.name,
+          data: value
+            .filter((f) => f[`@${series.id}`] === d.code)
+            .map((d) => {
+              return {
+                x: parseInt(d['@time'].substr(0, 4)),
+                y: parseFloat(d.$),
+                unit: d['@unit'],
+              }
+            }),
         }
       })
     },
     displayData() {
-      if (this.series === 'all') {
-        return this.chartData.filter(f => f.name === '総人口')
+      if (this.selectedSeries === 'all') {
+        return this.chartData.filter((f) => f.name === '総数')
       } else {
-        return this.chartData.filter(f => f.name !== '総人口')
+        return this.chartData.filter((f) => f.name !== '総数')
       }
-    }
+    },
+    chartComponent() {
+      switch (this.selectedChartType) {
+        case 'column':
+          return 'HighchartsColumnChart'
+          break
+        case 'line':
+          return 'HighchartsColumnChart'
+          break
+      }
+    },
   },
-  created() {}
 }
 </script>
